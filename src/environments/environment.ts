b13d9d9656cc1cdaa828ae54915963fbab92bb48
SaulMendozaB <U20211C677@upc.edu.pe>
2026-04-29 20:19:07 -0500

    -

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import { A } from "@angular/cdk/keycodes";
 
 export const environment = {
     production: true,
-    //apiUrl: 'https://edumath-api-c3hdh8fahga3bsfm.brazilsouth-01.azurewebsites.net'
-    apiUrl: 'http://127.0.0.1:8000'
+    apiUrl: 'https://edumath-api-c3hdh8fahga3bsfm.brazilsouth-01.azurewebsites.net'
+    //apiUrl: 'http://127.0.0.1:8000'
 };
